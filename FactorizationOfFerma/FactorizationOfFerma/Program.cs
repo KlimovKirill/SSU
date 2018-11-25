4ce3c04e4d8edecb28e6ae46b83c9b7699ff4f24
KlimovKirill <45049287+KlimovKirill@users.noreply.github.com>
2018-11-25 22:28:57 +0400

    .

---
+++ FactorizationOfFerma/FactorizationOfFerma/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorizationOfFerma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an odd number n > 1: n = ");
+
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            while (n % 2 == 0 || n < 1)
+            {
+                Console.Write("Your number is not odd or < 1. Enter a new one: n = ");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            //ищем наименьшее число, при котором x^2 - n является неотрицательным
+            int x = (int)Math.Sqrt(n);
+            //увеличиваем x на k=1,2,... пока не окажется точным квадратом
+            for (int q = x; x <= n; x++)
+            {               
+                double y = Math.Sqrt(Math.Pow(x, 2) - n);
+
+                //проверяем, является ли точным квадратом            
+                if (Math.Pow(y, 2) == Math.Pow(Math.Sqrt(Math.Pow(x, 2) - n), 2) && (int)y == y)
+                {
+                    Console.WriteLine($"n^2 = (x-y)*(x+y) = a*b where a = {x-y} b = {x+y}");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ TestOfRabenMiller/TestOfRabenMiller/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestOfRabenMiller
+{
+    class Program
+    {
+        public static bool Test_Miller_Rabin(ulong n)
+        {
+            // если n == 2 или n == 3 - эти числа простые, возвращаем true
+            if (n == 2 || n == 3)
+                return true;
+            // если n < 2 или n четное - возвращаем false
+            if (n % 2 == 0 || n < 2)
+            {
+                return false;
+            }
+
+            BigInteger x = 0;
+
+            ulong r1 = 2;
+            ulong r2 = n - 2;
+            ulong a;
+
+            ulong r = (ulong)(Math.Log(n) / Math.Log(2));
+
+            // представим n − 1 в виде (2^s)·t, где t нечётно, это можно сделать последовательным делением n - 1 на 2
+            ulong s = 0;
+            ulong t = n - 1;
+
+            while (t != 0 && t % 2 == 0)
+            {
+                s++;
+                t /= 2;
+            }
+
+            Random rand = new Random();
+
+            for (ulong i = 0; i < r; i++)
+            {
+                //a = r1 + ((ulong)rand.Next() % (r2 - r1));
+                a = (ulong)rand.Next((int)r1,(int)r2 );
+
+                // x ← a^t mod n, вычислим с помощью возведения в степень по модулю
+                x = BigInteger.ModPow(a, t, n);
+               
+                // если x == 1 или x == n − 1, то перейти на следующую итерацию цикла
+                if (x == 1 || x == n - 1)
+                {
+                    continue;
+                }
+
+                //повторяем s-1 раз
+                for (ulong j = 0; j < s - 1; j++)
+                {
+
+                    x = BigInteger.ModPow(a, 2, n);
+
+                    // если x == 1, то вернуть "составное"
+                    if (x == 1)
+                        return false;
+
+                    // если x == n − 1, то перейти на следующую итерацию внешнего цикла
+                    if (x == n - 1)
+                        break;
+                }
+
+                if (x == n - 1)
+                    continue;
+
+                return false;
+            }
+
+            // вернуть "вероятно простое"
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            ulong n = ulong.Parse(Console.ReadLine());
+
+            if (Test_Miller_Rabin(n))
+                Console.WriteLine("Possibly primary!");
+            else
+                Console.WriteLine("Composite!");
+            
+        }
+    }
+}
